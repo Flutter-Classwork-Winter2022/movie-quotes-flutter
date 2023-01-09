7f5e5ea8f6d14c2c84effae9d533e4c5eb621687
ChronicApp <koenignm@rose-hulman.edu>
2023-01-09 13:30:07 -0500

    Created content in list page

---
+++ lib/pages/movie_quotes_list_page.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+import '../models/movie_quote.dart';
+
+class MovieQuotesListPage extends StatefulWidget {
+  const MovieQuotesListPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MovieQuotesListPageState();
+  }
+
+}
+
+class _MovieQuotesListPageState extends State<MovieQuotesListPage> {
+  final quotes = <MovieQuote>[]; // Later, we will remove this and use the firestore
+
+  @override
+  void initState() {
+    super.initState();
+    quotes.add(
+        MovieQuote(
+            quote: "I'll be back",
+            movie: "The Terminator"
+        ));
+    quotes.add(
+        MovieQuote(
+            quote: "You killed my father, prepare to die.",
+            movie: "The Princess Bride"
+        ));
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Movie Quotes")
+      ),
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Text("Hello"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print("You pressed the FAB!");
+        },
+        tooltip: 'Create',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }}
